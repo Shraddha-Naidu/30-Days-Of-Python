d18a32b1ed4abe0438884cf36fbc66240b4bdbaf
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-07-09 22:04:30 -0600

    Add additional notes as comments

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -12,10 +12,29 @@ print(3 // 2)  # Floor division operator(//)
 # Checking data types
 
 print(type(10))                  # Int
+# integers are whole positive or negative numbers
+
 print(type(3.14))                # Float
+# Float numbers are decimals, positive or negative
+
 print(type(1 + 3j))              # Complex
+# Complex number is an equation with a variable
+
 print(type('Asabeneh'))          # String
+#A string is one or more characters within single or doulble quoutes
+
 print(type([1, 2, 3]))           # List
+# A list is an ordered collection and can be different types of data within the collection. Similar to arras in JS
+
 print(type({'name':'Asabeneh'})) # Dictionary
+# A dictionary object is an unordered collection of data in a key-value pair format
+
 print(type({9.8, 3.14, 2.7}))    # Set
+# A set of data is an unordered collection of data
+
 print(type((9.8, 3.14, 2.7)))    # Tuple
+# A tuple is similar to a list, the difference being that the collection of data is fixed and can not be altered.
+
+# Boolean is a data that either holds a True or False value
+
+# """triple quotes""" are used to indicate multi line text, if its not attached to avariable then its a comment
