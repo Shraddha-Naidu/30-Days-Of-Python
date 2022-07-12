9a200b38b62241f28b400a1a53b5abaebda9e3d2
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-07-11 20:07:48 -0600

    complete exercise 2 & 3

---
+++ 01_Day_Introduction/helloworld1.py
@@ -0,0 +1,33 @@
+## EXERCISE LEVEL 2 ##
+#Using operators
+from math import sqrt
+
+
+print(3 + 4) #addition - should return 7
+print(4 - 3) #subtraction - should return 1
+print(4 * 3) #multiplication - should return 12
+print(4 % 3) #modulus - returns remainder, 1 in this case
+print(4 ** 3) #exponential - to the power of, should return 64
+print(4 // 3) #floor dividioon operator - read more into it, don't get it yet
+
+#Printing strings
+print('Shraddha')
+print('Naidu')
+print('Canada')
+print("I am enjoying 30 days of python!")
+
+#Checking Value types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("SHraddha Naidu"))
+
+
+## Exercise Level 3 ##
+# Find an Euclidian distance between (2, 3) and (10, 8)
+
+print(sqrt((2 - 3) ** 2))
+print(sqrt((10 - 8) ** 2))
+
